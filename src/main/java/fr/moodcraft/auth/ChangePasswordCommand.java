a36c215a63d93753cdbfb416a3b1f9ff949f9828
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-10 19:28:03 +0200

    Update ChangePasswordCommand.java

--- src/main/java/fr/moodcraft/auth/ChangePasswordCommand.java
+++ src/main/java/fr/moodcraft/auth/ChangePasswordCommand.java
@@ -6,13 +6,30 @@ import org.bukkit.entity.Player;
 public class ChangePasswordCommand implements CommandExecutor {
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    public boolean onCommand(
+            CommandSender sender,
+            Command cmd,
+            String label,
+            String[] args
+    ) {
 
-        if (!(sender instanceof Player p)) return true;
+        if (!(sender instanceof Player p)) {
+            sender.sendMessage("§cCommande joueur uniquement.");
+            return true;
+        }
+
+        if (!AuthListener.isLogged(p)) {
+            p.sendMessage("§8----- §6Sécurité MoodCraft §8-----");
+            p.sendMessage("§c🔒 §fSession verrouillée.");
+            p.sendMessage("§7Connecte-toi avant de modifier ton mot de passe.");
+            p.sendMessage("§6➜ §e/login <motdepasse>");
+            return true;
+        }
 
         if (args.length < 2) {
-            p.sendMessage("§c⚠ Informations manquantes.");
-            p.sendMessage("§7Utilisation : §e/changepassword <ancien> <nouveau>");
+            p.sendMessage("§8----- §6Sécurité MoodCraft §8-----");
+            p.sendMessage("§c⚠ §fInformations manquantes.");
+            p.sendMessage("§7Utilisation : §e/changepass <ancien> <nouveau>");
             return true;
         }
 
@@ -23,13 +40,15 @@ public class ChangePasswordCommand implements CommandExecutor {
         );
 
         if (!success) {
-            p.sendMessage("§c❌ Ancien mot de passe incorrect.");
+            p.sendMessage("§8----- §6Sécurité MoodCraft §8-----");
+            p.sendMessage("§c✖ §fAncien mot de passe incorrect.");
+            p.sendMessage("§7Modification refusée.");
             return true;
         }
 
-        p.sendMessage("§a✔ Mot de passe mis à jour !");
-        p.sendMessage("§7Ton compte est maintenant sécurisé.");
-
+        p.sendMessage("§8----- §6Sécurité MoodCraft §8-----");
+        p.sendMessage("§a✔ §fMot de passe mis à jour.");
+        p.sendMessage("§7Ton compte est protégé.");
         return true;
     }
 }
